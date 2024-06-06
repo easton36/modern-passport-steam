c783752bad36aa3e0caa66c9603ea4aae087a3ad
Easton <easton@hey.com>
2024-06-06 13:43:11 -0600

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,82 @@
+module.exports = {
+	env: {
+		browser: false,
+		commonjs: true,
+		es2021: true
+	},
+	extends: [
+		'standard',
+		'plugin:security/recommended',
+		'plugin:require-path-exists/recommended'
+	],
+	plugins: [
+		'security',
+		'require-path-exists'
+	],
+	overrides: [
+	],
+	ignorePatterns: [
+		'node_modules',
+		'build',
+		'package-lock.json',
+		'package.json',
+		'*.json',
+		'logs/',
+		'*.log',
+		'*.prisma',
+		'*.dbml',
+		'*.sol'
+	],
+	parserOptions: {
+		ecmaVersion: 'latest'
+	},
+	rules: {
+		semi: [2, 'always'],
+		'func-call-spacing': [2, 'never'],
+		'keyword-spacing': [2, {
+			before: false,
+			after: false,
+			overrides: {
+				catch: { before: true, after: false },
+				const: { before: false, after: true },
+				return: { before: true, after: true },
+				case: { before: false, after: true },
+				else: { before: true, after: false },
+				async: { before: false, after: true },
+				of: { before: true, after: true },
+				continue: { before: true, after: false },
+				throw: { before: true, after: true },
+				await: { before: true, after: true },
+				break: { before: true, after: false },
+				finally: { before: true, after: false }
+			}
+		}],
+		'space-before-blocks': [2, 'never'],
+		'newline-before-return': 2,
+		'eol-last': [2, 'never'],
+		indent: ['error', 'tab'],
+		'no-tabs': 0,
+		camelcase: 0,
+		'prefer-regex-literals': 0,
+		'no-useless-escape': 0,
+		'no-unused-vars': ['error', { args: 'none' }],
+		'n/no-callback-literal': 0,
+		'space-before-function-paren': ['error', {
+			anonymous: 'never',
+			named: 'never',
+			asyncArrow: 'always'
+		}],
+		'no-return-assign': 0,
+		'security/detect-object-injection': 0,
+		'max-params': ['error', 4],
+		'no-new': 0,
+		'no-param-reassign': ['error', { props: false }]
+	}
+};
+
+/*
+ Classes should use PascalCase
+ Functions should use camelCase
+ Variables should use camelCase
+ Constants should use SCREAMING_SNAKE_CASE (only very important constants)
+*/
---
+++ src/strategy.js
@@ -0,0 +1,67 @@
+const { Strategy } = require('passport-strategy');
+const assert = require('assert');
+
+const {
+	verifyLogin,
+	buildAuthUrl,
+	canonicalizeRealm
+} = require('./helpers');
+
+/**
+ * `SteamStrategy` is a class that extends `Strategy` for Steam authentication.
+ * @class
+ * @extends Strategy
+ *
+ * @example
+ * const strategy = new SteamStrategy({
+ *   realm: 'http://yourdomain.com/',
+ *   returnUrl: 'http://yourdomain.com/auth/steam/return'
+ * }, (req, SteamID, done) => {
+ *   // do something with the SteamID object
+ * });
+ */
+class SteamStrategy extends Strategy{
+	/**
+     * Creates a new `SteamStrategy`.
+     * @param {Object} options - The options for the strategy.
+     * @param {string} options.realm - The realm for the strategy.
+     * @param {string} options.returnUrl - The return URL for the strategy.
+     * @param {Function} verify - The verify callback.
+     */
+	constructor(options, verify){
+		super();
+		this.name = 'steam';
+		this._verify = verify;
+		this._realm = canonicalizeRealm(options.realm);
+		this._returnUrl = options.returnUrl;
+	}
+
+	/**
+	 * Authenticate the user
+	 * @param {Object} req - The express request object
+	 * @returns {Promise<void>}
+	 */
+	async authenticate(req){
+		if(req.query && req.query['openid.mode']){
+			try{
+				const userSteamId = await verifyLogin(req.url, this._realm);
+				assert(userSteamId, 'Steam validation failed');
+
+				this._verify(req, userSteamId, (err, user) => {
+					if(err){
+						return this.error(err);
+					}
+
+					return this.success(user);
+				});
+			} catch(err){
+				return this.fail(err);
+			}
+		} else{
+			const authUrl = buildAuthUrl(this._realm, this._returnUrl);
+			this.redirect(authUrl);
+		}
+	}
+}
+
+module.exports = SteamStrategy;
